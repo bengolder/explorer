fe6b69b46ada6571b2a4f13fa52873a112cf1a25
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-03-24 14:31:25 -0400

    quick edit to admin

--- explorer/mit/admin.py
+++ explorer/mit/admin.py
@@ -14,15 +14,22 @@ from mit.models import (
         Location,
         )
 
-admin.site.register(Person)
-admin.site.register(Faculty)
-admin.site.register(Work)
+class FacultyAdmin(admin.ModelAdmin):
+    fields = ('full_name', 'official_title', 'current_interests', 'email',
+    'home_page', 'bio', 'places_lived')
+
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Person)
+admin.site.register(Faculty, FacultyAdmin)
+# admin.site.register(Work)
 admin.site.register(Project)
 admin.site.register(Publisher)
 admin.site.register(Publication)
 admin.site.register(Book)
 admin.site.register(Article)
 admin.site.register(JournalArticle)
-admin.site.register(Topic)
+admin.site.register(Topic, TopicAdmin)
 admin.site.register(Location)
 
