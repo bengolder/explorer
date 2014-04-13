be6b72663372f491c4d427ae09c0fd7247ea50cc
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-04-13 15:41:39 -0400

    added polymorphic serialization

--- explorer/mit/serializers.py
+++ explorer/mit/serializers.py
@@ -16,6 +16,42 @@ from mit.models import (
         )
 
 
+class FacultySerializer(ModelSerializer):
+    class Meta:
+        model = Faculty
+
+class ResearchInitiativeSerializer(ModelSerializer):
+    class Meta:
+        model = ResearchInitiative
+
+class ColloquiumSerializer(ModelSerializer):
+    class Meta:
+        model = Colloquium
+
+class JournalArticleSerializer(ModelSerializer):
+    class Meta:
+        model = JournalArticle
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+
+class BookSerializer(ModelSerializer):
+    class Meta:
+        model = Book
+
+class PublicationSerializer(ModelSerializer):
+    class Meta:
+        model = Publication
+
+class ProjectSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+
+class WorkSerializer(ModelSerializer):
+    class Meta:
+        model = Work
+
 class TopicSerializer(ModelSerializer):
     class Meta:
         model = Topic
@@ -24,17 +60,29 @@ class LocationSerializer(ModelSerializer):
     class Meta:
         model = Location
 
-class FacultySerializer(ModelSerializer):
-    class Meta:
-        model = Faculty
 
 class WorkSerializer(ModelSerializer):
     class Meta:
         model = Work
 
-class ResearchInitiativeSerializer(ModelSerializer):
-    class Meta:
-        model = ResearchInitiative
+    def to_native(self, obj):
+        if isinstance(obj, ResearchInitiative):
+            return ResearchInitiativeSerializer(obj).to_native(obj)
+        elif isinstance(obj, Colloquium):
+            return ColloquiumSerializer(obj).to_native(obj)
+        elif isinstance(obj, JournalArticle):
+            return JournalArticleSerializer(obj).to_native(obj)
+        elif isinstance(obj, Article):
+            return ArticleSerializer(obj).to_native(obj)
+        elif isinstance(obj, Book):
+            return BookSerializer(obj).to_native(obj)
+        elif isinstance(obj, Publication):
+            return PublicationSerializer(obj).to_native(obj)
+        elif isinstance(obj, Project):
+            return ProjectSerializer(obj).to_native(obj)
+        else:
+            return super(WorkSerializer, self).to_native(obj)
+
 
 
 
