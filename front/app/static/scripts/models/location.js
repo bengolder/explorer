8faf5607551c3b511f7a2c2a2f7289e5864c255e
Ben Golder <benjamin.j.golder@gmail.com>
2014-04-22 11:27:34 -0400

    added location model

---
+++ front/app/static/scripts/models/location.js
@@ -0,0 +1,28 @@
+define([
+], function () {
+var LocationBase = {
+// Available from inheritance:
+	// collection
+	// displayKey
+
+// topics with similar works
+spans: {
+	'related_topics': {
+		'bridge_attribute': 'works',
+		'other_attribute': 'topics',
+	},
+	'related_faculty': {
+		'bridge_attribute': 'works',
+		'other_attribute': 'authors',
+	},
+	'related_locations': {
+		'bridge_attribute': 'works',
+		'other_attribute': 'locations',
+	},
+},
+
+};
+return LocationBase;
+});
+
+
