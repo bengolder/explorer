cdb572c8b7c79c842355158fa4158b8c38e18042
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-06-09 13:47:12 -0700

    created color scale and drag behavior for globe

--- front/app/static/scripts/views/globe.js
+++ front/app/static/scripts/views/globe.js
@@ -6,8 +6,6 @@ define([
 'appconfig',
 'topojson'
 ], function( Events, Data, BB, d3, config, topojson){
-
-
 var GlobeView = BB.View.extend({
 
 className: 'chart',
@@ -23,8 +21,107 @@ initialize: function(data){
 
 },
 
+drawGlobe: function(){
+	var me = this;
+	
+	var c = this.context;
+	// this is meant to be called initially, and by tweens
+	// clear the canvas context
+	c.clearRect(0, 0, this.w, this.h);
+	// fill for all the unselected countries
+	this.drawFill("#aaa", this.land);
+	// fill for the selected country
+	this.data.forEach(function (d){
+		// get corresponding color
+		var fill = me.colorScale(d.get('works').length);
+		// get corresponding shape
+		me.countries.features.forEach(function(c){
+			if( c.id == d.get('official_id') ){
+				me.drawFill( fill, c );
+			}
+		});
+	});
+	// This next line will break
+	this.drawStroke("fff", 0.5, this.borders);
+	// outline for the globe itself
+	this.drawStroke("#000", 2, this.globe);
+
+},
+
+initCanvas: function(){
+
+	var w = this.$el.width();
+	var h = this.$el.height();
+	this.w = w;
+	this.h = h;
+	var innerRadius = Math.min(w, h) * 0.35;
+	var outerRadius = innerRadius * 1.05;
+
+    this.projection = d3.geo.orthographic()
+        .scale(248)
+        .clipAngle(90);
+	console.log("projection", this.projection);
+	console.log("projection origin", this.projection.center());
+
+    var circumScale = d3.scale.linear()
+        .domain([-(w/2), w/2])
+        .range([-210, 210]);
+
+    var pitchScale = d3.scale.linear()
+        .domain([-(h/2), h/2])
+        .range([60, -60]);
+
+	var div = d3.select(this.el);
+	this.canvas = div.append('canvas')
+		.attr('width', w)
+		.attr('height', h);
+
+	this.context = this.canvas.node().getContext('2d');
+
+	var me = this;
+
+    //this.canvas.on("mousemove", function() {
+
+        //var p = d3.mouse(this);
+
+        //me.projection.rotate([
+			//circumScale(p[0]), 
+			//pitchScale(p[1])
+			//]);
+
+        //me.drawGlobe();
+    //});
+
+	var me = this;
+
+	this.drag = d3.behavior.drag()
+		.on('drag', function(d){
+			if ( me.mouseStart ) {
+				var mouse = d3.mouse(me.canvas.node());
+				var dx = mouse[0] - me.mouseStart[0];
+				var dy = mouse[1] - me.mouseStart[1];
+
+				var yaw = me.currentRotation[0] + circumScale(dx);
+				var pitch = me.currentRotation[1] + pitchScale(dy);
+				me.projection.rotate([yaw, pitch, 0]);
+				me.drawGlobe();
+			}
+		}).on('dragstart', function(d){
+			me.currentRotation = me.projection.rotate();
+			me.mouseStart = d3.mouse(me.canvas.node());
+		});
+
+	this.canvas.on('click', function(d){
+		var mouse = d3.mouse(me.canvas.node());
+		var lonlat = me.projection.invert(mouse);
+	});
+
+	this.canvas.call(this.drag);
+},
+
 render: function(data){
 	var me = this;
+
 	if( !this.ready ){
 		console.log("waiting for globe data");
 		Events.on('globeViewReady', function(){
@@ -32,82 +129,51 @@ render: function(data){
 		});
 		return;
 	}
-	console.log("starting globe", Data.globe);
+	
+	var maxProjects = d3.max(data, function(d){ return d.get('works').length; });
+	this.colorScale = d3.scale.linear()
+		.domain([1, maxProjects])
+		.range(['#c6dbef','#3182bd']);
 
-	var w = this.$el.width();
-	var h = this.$el.height();
-	var innerRadius = Math.min(w, h) * 0.35;
-	var outerRadius = innerRadius * 1.05;
-	var div = d3.select(this.el);
+	this.initCanvas();
+
+	console.log("starting globe", Data.globe);
 
 	var world = Data.globe;
 
-    var projection = d3.geo.orthographic()
-        .scale(248)
-        .clipAngle(90);
+    this.path = d3.geo.path()
+        .projection(this.projection)
+        .context(this.context);
 
-	var canvas = div.append('canvas')
-		.attr('width', w)
-		.attr('height', h);
+    this.globe = {type: "Sphere"};
 
-	var c = canvas.node().getContext('2d');
+    this.land = topojson.feature(world, world.objects.land);
+	this.countries = topojson.feature( world, world.objects.countries );
+    this.borders = topojson.mesh(world, world.objects.countries, 
+			function(a, b) { return a.id !== b.id; });
+	this.data = data;
+	this.drawGlobe();
 
-    canvas.on("mousemove", function() {
-        var p = d3.mouse(this);
-        projection.rotate([circumScale(p[0]), pitchScale(p[1])]);
-        drawGlobe();
-    });
-
-    var circumScale = d3.scale.linear()
-        .domain([0, w])
-        .range([-210, 210]);
-
-     var pitchScale = d3.scale.linear()
-         .domain([0, h])
-         .range([60, -60]);
-
-    var path = d3.geo.path()
-        .projection(projection)
-        .context(c);
-
-    var globe = {type: "Sphere"},
-      land = topojson.feature(world, world.objects.land),
-      countries = topojson.feature(world, world.objects.countries).geometries,
-      borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a.id !== b.id; });
-
-	function drawGlobe(){
-        // this is meant to be called initially, and by tweens
-        // clear the canvas context
-        c.clearRect(0, 0, w, h);
-        // fill for all the unselected countries
-        drawFill("#aaa", land);
-        // fill for the selected country
-        // This next line will break
-        drawStroke("fff", 0.5, borders);
-        // outline for the globe itself
-        drawStroke("#000", 2, globe);
-	}
-
-    function drawFill( color, pathItems){
-        c.fillStyle = color;
-        c.beginPath();
-        path(pathItems);
-        c.fill();
-    }
-
-    function drawStroke( color, strokeWidth, pathItems ){
-        c.strokeStyle = color;
-        c.lineWidth = strokeWidth;
-        c.beginPath();
-        path(pathItems);
-        c.stroke();
-    }
-
-	drawGlobe();
 },
 
+drawFill: function( color, pathItems){
+	var c = this.context;
+	c.fillStyle = color;
+	c.beginPath();
+	this.path(pathItems);
+	c.fill();
+},
+
+drawStroke: function drawStroke( color, strokeWidth, pathItems ){
+	var c = this.context;
+	c.strokeStyle = color;
+	c.lineWidth = strokeWidth;
+	c.beginPath();
+	this.path(pathItems);
+	c.stroke();
+},
+
+
 });
-
-
 return GlobeView;
 });
