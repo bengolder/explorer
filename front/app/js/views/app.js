95b04dfed089d42fcfeb09afcaf91425ae032aa4
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-08-13 18:45:52 -0400

    added info dropdown

--- front/app/js/views/app.js
+++ front/app/js/views/app.js
@@ -1,8 +1,8 @@
 // imports
 var BB = require('backbone');
+var d3 = require('d3');
 BB.$ = window.$;
 var QueryView = require('../views/query');
-var template = require('../templates/main.html');
 
 var AppView = BB.View.extend({
 
@@ -11,6 +11,8 @@ var AppView = BB.View.extend({
 	initSubViews: function () {
 		this.queryView = new QueryView();
 		this.$el.find('#controls').append(this.queryView.$el);
+		this.$el.on('click', '.info-button', this.makeInfoToggler());
+		// add click callback to info button
 	},
 
 
@@ -18,17 +20,32 @@ var AppView = BB.View.extend({
 	    // bind events, 
 		// render self, 
 		// then initialize children
-		this.render();
 		this.initSubViews();
 	},
 
-	render: function () {
-		// renders will often be split into subfunctions
-		var that = this;
-		this.$el.html(template);
-		// Later we will implement history
-		// Backbone.history.start();
+	makeInfoToggler: function(){
+		var panel = $("#info");
+		var states = { isOpen: false };
+		var p = d3.selectAll("#info p");
+		p.style("color", "#fff");
+		return function(e){
+			panel.slideToggle(300);
+			if( states.isOpen ){
+				console.log("goodbye", states);
+				p.transition()
+					.duration(500)
+					.style("color", "white");
+				states.isOpen = false;
+			} else {
+				console.log("hello", states);
+				p.transition()
+					.duration(1400)
+					.style("color", "black");
+				states.isOpen = true;
+			}
+		};
 	},
+
 });
 
 module.exports = AppView;
